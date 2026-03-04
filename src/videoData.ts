b37f7859f782c96573e20395bbb94c7f7953f041
ManeeshWije <m.mwije1@proton.me>
2026-03-03 22:21:30 -0500

    update vids

--- src/videoData.ts
+++ src/videoData.ts
@@ -1,59 +1,51 @@
 export const videos = [
     {
+        title: "(TB2) I Am Made Of Chalk v10 @ 40°",
+        youtubeId: "tPaMNM-Fzrc"
+    },
+    {
         title: "(TB2) The Silver Mountain v10 @ 40°",
         youtubeId: "lI3h1sIJa0U"
     },
     {
-        title: "v10+ @ aspire 2",
+        title: "v10+ @ aspire",
         youtubeId: "K6ixlZB7dxs"
     },
     {
-        title: "v10+ @ aspire 2 (soft)",
+        title: "v10+ @ aspire (soft)",
         youtubeId: "jbQi2_Qpy3c",
     },
     {
-        title: "v8-v9 @ aspire 2",
+        title: "v8-v9 @ aspire",
         youtubeId: "U364pXJ9E84"
     },
     {
-        title: "v8-v9 @ aspire 2",
+        title: "v8-v9 @ aspire",
         youtubeId: "JXaJ_FPmlbA"
     },
     {
-        title: "v8-v9 @ aspire 2",
+        title: "v8-v9 @ aspire",
         youtubeId: "1kN0f2_NCV4"
     },
     {
-        title: "v6-v7 @ aspire 2",
+        title: "v6-v7 @ aspire",
         youtubeId: "wgbU87khqfM"
     },
     {
-        title: "v8-v9 @ aspire 2",
+        title: "v8-v9 @ aspire",
         youtubeId: "er0-n5akMDw",
     },
     {
-        title: "v8-v9 @ aspire 2",
+        title: "v8-v9 @ aspire",
         youtubeId: "3-gCI4bch54",
     },
     {
-        title: "v5 campus @ aspire 2 (fail)",
+        title: "v5 campus @ aspire (fail)",
         youtubeId: "4yx3TEhUXj4",
     },
     {
         title: "v10+ @ aspire (soft)",
         youtubeId: "N4m7oUkuV_s",
-    },
-    {
-        title: "v8-v9 @ aspire",
-        youtubeId: "DErK5Uy2YPM",
-    },
-    {
-        title: "v6-v7 @ aspire",
-        youtubeId: "V7GspeNw-08",
-    },
-    {
-        title: "v8-v9 @ aspire",
-        youtubeId: "4AP6Nd_dt7w",
     },
     {
         title: "purple tape @ grotto",
