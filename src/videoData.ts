ddde9d1580d6c799dd3ffcdac31b01031dad1695
ManeeshWije <m.mwije1@proton.me>
2026-03-15 22:57:25 -0400

    update vids

--- src/videoData.ts
+++ src/videoData.ts
@@ -1,5 +1,21 @@
 export const videos = [
     {
+        title: "(TB2) Marceline v10 @ 45°",
+        youtubeId: "OZMiE1Li9E0"
+    },
+    {
+        title: "(TB2) yaoooo v10 @ 45°",
+        youtubeId: "X_FbQYBysGY"
+    },
+    {
+        title: "(TB2) Pagan Practice v10 @ 50°",
+        youtubeId: "VpUk93uWxoE"
+    },
+    {
+        title: "(TB2) Miami Vice v10 @ 50°",
+        youtubeId: "zaWTx7Tx3po"
+    },
+    {
         title: "(TB2) Colder Shoulder v10 @ 40°",
         youtubeId: "QOHjUhEoasM",
     },
@@ -16,28 +32,12 @@ export const videos = [
         youtubeId: "K6ixlZB7dxs"
     },
     {
-        title: "v10+ @ aspire (soft)",
-        youtubeId: "jbQi2_Qpy3c",
-    },
-    {
-        title: "v8-v9 @ aspire",
-        youtubeId: "U364pXJ9E84"
-    },
-    {
-        title: "v8-v9 @ aspire",
-        youtubeId: "JXaJ_FPmlbA"
-    },
-    {
         title: "v8-v9 @ aspire",
         youtubeId: "1kN0f2_NCV4"
     },
     {
         title: "v6-v7 @ aspire",
         youtubeId: "wgbU87khqfM"
-    },
-    {
-        title: "v8-v9 @ aspire",
-        youtubeId: "3-gCI4bch54",
     },
     {
         title: "v5 campus @ aspire (fail)",
