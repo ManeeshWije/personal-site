811600e01c9a7d0c7822731a16805674097db10e
ManeeshWije <m.mwije1@proton.me>
2026-08-03 15:48:04 -0400

    use flake

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Personal Website
+
 - An interactive terminal style website where people can gain more info about me, written in TypeScript React.
+
+### Developing
+
+This project uses nix flakes for development. To get started, you can run the following command to enter a development shell:
+
+```bash
+nix develop
+```
+
+Within the development shell, you can run the following command to start the development server:
+
+```bash
+npm run dev
+```
+
+For deployment, we use nix2container to build and push the docker image to dockerhub
--- flake.nix
+++ flake.nix
@@ -1,31 +1,120 @@
 {
-  inputs = {
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  description = "Personal website container image";
 
-    # Support a particular subset of the Nix systems
-    # systems.url = "github:nix-systems/default";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix2container = {
+      url = "github:nlewo/nix2container";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    { nixpkgs, ... }:
-    let
-      eachSystem =
-        f:
-        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
-          system: f system nixpkgs.legacyPackages.${system}
-        );
-    in
-    {
-      devShells = eachSystem (
-        _system: pkgs: {
-          default = pkgs.mkShell {
-            packages = [
-              pkgs.nodejs
-              pkgs.corepack
-              pkgs.typescript
-            ];
-          };
-        }
+  outputs = {
+    nixpkgs,
+    nix2container,
+    ...
+  }: let
+    systems = [
+      "x86_64-linux"
+    ];
+
+    eachSystem = f:
+      nixpkgs.lib.genAttrs systems (
+        system:
+          f {
+            inherit system;
+            pkgs = nixpkgs.legacyPackages.${system};
+            nix2container =
+              nix2container.packages.${system}.nix2container;
+          }
       );
-    };
+  in {
+    packages = eachSystem (
+      {
+        pkgs,
+        nix2container,
+        ...
+      }: let
+        site = pkgs.buildNpmPackage {
+          pname = "personal-website";
+          version = "0.0.0";
+
+          src = pkgs.lib.cleanSource ./.;
+
+          # Run `nix build .#site` once and replace this with the
+          # sha256 hash shown in the error message.
+          npmDepsHash = "sha256-x43jpFSVPnTypnC2AhnQdJoQadQKy9ODBZBxBI5xJRE=";
+
+          npmBuildScript = "build";
+
+          installPhase = ''
+            runHook preInstall
+
+            mkdir -p $out/app
+
+            cp package.json $out/app/
+            cp -r dist $out/app/
+
+            # Vite is a dev dependency, but it is required at runtime
+            # because the container runs `npm run preview`.
+            cp -RL node_modules $out/app/node_modules
+
+            runHook postInstall
+          '';
+        };
+
+        image = nix2container.buildImage {
+          name = "docker.io/maneeshwije/personal-website";
+          tag = "latest";
+
+          copyToRoot = site;
+
+          config = {
+            WorkingDir = "/app";
+
+            Cmd = [
+              "${pkgs.nodejs}/bin/npm"
+              "run"
+              "preview"
+              "--"
+              "--host"
+              "0.0.0.0"
+              "--port"
+              "4173"
+            ];
+
+            Env = [
+              "HOME=/tmp"
+              "NODE_ENV=production"
+            ];
+
+            ExposedPorts = {
+              "4173/tcp" = {};
+            };
+
+            Labels = {
+              "org.opencontainers.image.source" = "https://github.com/ManeeshWije/personal-site";
+            };
+          };
+        };
+      in {
+        default = image;
+
+        inherit site image;
+      }
+    );
+
+    devShells = eachSystem (
+      {pkgs, ...}: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs
+            pkgs.corepack
+            pkgs.typescript
+          ];
+        };
+      }
+    );
+  };
 }
